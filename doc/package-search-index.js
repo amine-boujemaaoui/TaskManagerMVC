3fcce4141acf041f85e656b8648ca3437ab75af0
Amine Boujemaaoui <92468393+amine-boujemaaoui@users.noreply.github.com>
2025-01-26 16:27:48 +0100

    generated html javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controllers"},{"l":"interfaces"},{"l":"models.Entities"},{"l":"models.Repositories"},{"l":"utils"},{"l":"views"}];updateSearchResults();
